0be4a65e3ca31ab462bd91666e3532b58b9300f7
Jovana Sobo <sobojovana@gmail.com>
2019-06-27 22:08:51 +0200

    Header

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+
+    $(window).on("scroll", function () {
+        if ($(window).scrollTop() > 50) {
+            $(".pp-l-header").addClass("pp-l-header--scroll");
+        } else {
+            $(".pp-l-header").removeClass("pp-l-header--scroll");
+        }
+    });
+});
