c95911b717df9da035de3565b317a1a594df05e8
Jovana Sobo <sobojovana@gmail.com>
2019-07-02 10:55:10 +0200

    Careers section

--- js/script.js
+++ js/script.js
@@ -1,11 +1,28 @@
-/*$(document).ready(function () {
+$(document).ready(function () {
 
 
-    $(window).on("scroll", function () {
+    /*$(window).on("scroll", function () {
         if ($(window).scrollTop() > 50) {
             $(".pp-l-header").addClass("pp-l-header--scroll");
         } else {
             $(".pp-l-header").removeClass("pp-l-header--scroll");
         }
+    });*/
+
+    $('.pp-c-careers-dropdown').each(function () {
+        var $dropdown = $(this);
+
+        $(".pp-c-careers-dropdown__link", $dropdown).click(function (e) {
+            e.preventDefault();
+            $div = $(".pp-c-careers-dropdown__container", $dropdown);
+            $div.toggle();
+            $(".pp-c-careers-dropdown__container").not($div).hide();
+            return false;
+        });
+
     });
-}); */
+
+    $('html').click(function () {
+        $(".pp-c-careers-dropdown__container").hide();
+    });
+});
